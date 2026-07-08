62f2e6300b257ba9a1c86b5dbf510d72b9682641
rasyidamn <132162495+rasyidamn@users.noreply.github.com>
2026-07-08 17:49:07 +0700

    perbaikan routing

--- frontend-feature-based/src/routes/_guest.auth/index.tsx
+++ frontend-feature-based/src/routes/_guest.auth/index.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_guest/auth/")({
-	beforeLoad: () => redirect({ to: "/auth/login" }),
+	beforeLoad: () => {
+		throw redirect({ to: "/auth/login" });
+	},
 });
